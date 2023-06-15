0cfc28bd8ec06b96fd2233f024a353e9a1338a34
swatidurgekar <swatidurgekar@gmail.com>
2023-06-15 17:42:40 +0530

    Signup Screen

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -9,11 +9,38 @@ const SignUp = () => {
   const password = useRef();
   const confirmPassword = useRef();
 
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBDKpPV_JJK_GMvdVloHRyVu4G3Tjs2OaA",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    ).then((res) => {
+      if (res.ok) {
+        console.log("user has successfully signed up");
+      } else {
+        res.json().then((data) => {
+          alert(data.error.message);
+        });
+      }
+    });
+  };
   return (
     <div>
       <div className="form-div">
         <h2 className="signUp-header">{isLogin ? "Login" : "Sign Up"}</h2>
-        <Form>
+        <Form onSubmit={submitHandler}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email</Form.Label>
             <Form.Control ref={email} type="email" required />
